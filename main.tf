8bb1c2ca643d54ad679be49154163148faef9e30
lata-sagnali <49862826+lata-sagnali@users.noreply.github.com>
2019-06-14 16:03:49 +0530

    Updated with Disk

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
-resource "google_compute_network" "vpc_network" {
-  name = "vpc-network"
-}
+variable "zone" {
+  default = "us-central1-a"
+} 
+provider "google" {
+  project     = "beaming-inn-237311"
+  region      = "us-central1"
+  version     = "1.19.0"
+} 
+
+resource "google_compute_disk" "default" {
+  name  = "test-disk"
+  type  = "pd-ssd"
+  zone  = "${var.zone}" 
+  image = "debian-8-jessie-v20170523"
+  labels = {
+    environment = "dev"
+  }
+  physical_block_size_bytes = 4096
+} 
